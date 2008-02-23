3e205d1206e82c14e9999cf351e774752ff9fb46
Antoine Toulme <antoine@lunar-ocean.com>
2008-02-23 21:26:53 +0000

    added assertion
    git-svn-id: http://manifest.rubyforge.org/svn/trunk/manifest@4 9304de6f-5db2-47b2-8f37-0218b1ca5ad2

--- test/test_manifest.rb
+++ test/test_manifest.rb
@@ -6,7 +6,7 @@ describe "Able to parse a manifest" do
     File.open("manifest_org.eclipse.compare.MF") do |file|
       manifest = Manifest.new(file.read)
       manifest.sections.first["Manifest-Version"].keys[0].should == "1.0"
-      puts manifest.sections.first["Bundle-SymbolicName"]["org.eclipse.compare"]["singleton"]
+      manifest.sections.first["Bundle-SymbolicName"]["org.eclipse.compare"]["singleton"].should == "true"
     end
   end
 
